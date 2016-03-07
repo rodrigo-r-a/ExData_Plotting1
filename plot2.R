1d3df9fc969ee9005b8899f82e31a49c4d021761
rodrigo-r-a <rodrigorivera@ufm.edu>
2016-03-06 19:52:25 -0600

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,30 @@
+#Course Project 1
+#Plot 2
+#Download the dataset
+if(!file.exists("cp1d1.zip")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
+dateDownloadedcp1d1 <- date()
+#create the variables to use
+gap <- read.table(file, header=T, sep=";")
+gap$Date <- as.Date(gap$Date, format="%d/%m/%Y")
+df <- gap[(gap$Date=="2007-02-01") | (gap$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+#Create plot
+plot2 <- function(x,y){
+  plot(x,y, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot2.png", width=480, height=480)
+  dev.off()
+  cat("File saved in ", getwd())
+}
+plot2(df$timestamp, df$Global_active_power)
